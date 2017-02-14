89d44614764bd29662613d7816e038f7a70fde2b
Michal Siemaszko <mhsiemaszko@users.noreply.github.com>
2017-02-15 00:08:29 +0100

    Install Microsoft Azure Resource Manager Module

---
+++ 201-vm-winrm-windows/scripts/AzureInstall.ps1
@@ -0,0 +1,5 @@
+##################################################
+# Install Microsoft Azure Resource Manager Module 
+
+Write-Host "Installing Microsoft Azure Resource Manager modules ..."
+Install-Module AzureRM
