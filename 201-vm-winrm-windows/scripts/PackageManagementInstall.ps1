908b3c8e82f8516c2ffc8d87a2be40a2b5d85fc1
Michal Siemaszko <mhsiemaszko@users.noreply.github.com>
2017-02-15 00:07:37 +0100

    Install "PackageManagement PowerShell Modules"

---
+++ 201-vm-winrm-windows/scripts/PackageManagementInstall.ps1
@@ -0,0 +1,16 @@
+################################################# 
+# Install "PackageManagement PowerShell Modules"
+Write-Host "Installing 'PackageManagement PowerShell Modules' ..."
+$tempDirectory = "$env:SystemDrive\temp"
+New-Item "$tempDirectory" -type directory
+Invoke-WebRequest https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x64.msi -outfile "$tempDirectory\PackageManagement_x64.msi"
+Start-Process "$tempDirectory\PackageManagement_x64.msi" -ArgumentList '/qn' -Wait
+Remove-Item "$tempDirectory" -recurse
+
+# Install NuGet (or as part of provisioning VM)
+Write-Host "Installing 'NuGet' ..."
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 –Force –Verbose
+
+# Modify installation policy for 'PowerShellGallery' repository
+Write-Host "Modifying installation policy for 'PowerShellGallery' repository ..."
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
